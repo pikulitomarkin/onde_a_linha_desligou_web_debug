e2b75247a38d133c4956be03ec8687d02932f1d7
pikulitomarkin <mpadilha932@gmail.com>
2025-06-17 19:02:52 -0300

    Update kmweb.py

--- kmweb.py
+++ kmweb.py
@@ -328,6 +328,6 @@ def visualizar_mapa():
 def serve_static(filename):
     return send_from_directory(app.config['STATIC_FOLDER'], filename)
 
-if __name__ == "__main__":
-    
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    debug_mode = os.getenv('FLASK_DEBUG', 'false').lower() == 'true'
+    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
